d4962fad4b9fde224ad88e3683ec80bf7c59a286
Chris Neal <chrisvneal@gmail.com>
2019-03-03 21:59:56 -1000

    Changed browserSync settings

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,45 @@ const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 const scss_folder = './src/scss/**/*.scss';
-const src_html = './src/**/*.html';
+const src_html = './src/*.html';
 
-// refresh the browser 
-gulp.task('browserSync', function() {
+// refresh the browser
+
+function browser_sync() {
   browserSync.init({
     server: {
       baseDir: 'dist'
     }
   })
-})
+}
 
 // convert scss to css (dist)
 
-gulp.task('scss', function() {
+
+
+function scss() {
   return gulp.src(scss_folder)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist/css'))
-});
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+  }
+
+
 
 // copy html files to dist folder
 
-gulp.task('html', function() {
+
+function html() {
   return gulp.src(src_html)
     .pipe(gulp.dest('dist'))
-});   
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
 
 // convert images and copy to img folder 
 
@@ -45,14 +57,27 @@ gulp.task('html', function() {
 
 // watch sass, javascript, and html changes
 
-gulp.task('watch', function() {
+gulp.task('watch', gulp.series(html, scss, browser_sync, function() {
   
   // watch scss files for changes
-  gulp.watch(scss_folder, gulp.series('scss'));
+  gulp.watch(scss_folder, scss);
 
   // watch html files for changes
-  gulp.watch(src_html, gulp.series('html'));
-});
+  gulp.watch(src_html, html);
+}));
+
+
+
+
+
+
+
+
+
+
+
+exports.scss = scss;
+exports.html = html;
 
 
 

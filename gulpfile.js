51cd587df4095fef9329d9c576d882b9b7f7f206
Chris Neal <chrisvneal@gmail.com>
2019-03-03 17:01:37 -1000

    Edited gulp files

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Variables
+const gulp = require('gulp');
+
+
+// convert sass to css 
+
+// copy html files to dist    
+
+// convert images and copy to img folder 
+
+// refresh the browser //#endregion
+
+// watch sass, javascript, and html changes
+
+
+
+
+
